28ea19fc5e277ca67fffb97696808ab46464b80c
Muhammad Umer <umer_kk96@hotmail.com>
2016-01-31 18:09:29 -0500

    Tower Class & Tower Attribute Doc

---
+++ src/Tower.java
@@ -0,0 +1,16 @@
+
+public class Tower {
+
+	private String _name;
+	private float _buyingCost;
+	private float _refundValue;
+	private int _rangeInBlocks; //Range per blocks
+	private float _powerOfBullet; //Rate of Health to deduct from the target
+	private int _rateOfFire; // Interval between each fire
+	private int _health; //Tower health
+	
+	
+	
+	
+	
+}
