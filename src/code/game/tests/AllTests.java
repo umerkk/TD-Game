10993e47b5c0647305e9ffbf81ab280055695b3c
Lokesh Parappurath <l_parapp@encs.concordia.ca>
2016-04-07 00:04:03 -0400

    Adding test suit

--- src/code/game/tests/AllTests.java
+++ src/code/game/tests/AllTests.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ CritterFactoryTest.class, GameMapTest.class, MapLoggerTest.class, SingleGameControllerTest.class,
-		StrategyNearestTest.class, StrategyStrongestTest.class, StrategyWeakestTest.class, TDGameMain2Test.class,
-		UtilTest.class })
+		StrategyNearestTest.class, StrategyNearestToEndPointTest.class, StrategyStrongestTest.class,
+		StrategyWeakestTest.class, TDGameMain2Test.class, UtilTest.class })
 public class AllTests {
 
 }
